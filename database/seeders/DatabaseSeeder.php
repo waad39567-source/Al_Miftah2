90652ce16b726ad5bc0467a8e31fde7a4ed90c09
waad <waad87770@gmail.com>
2026-03-13 20:38:24 +0300

    Fix seeder order - run regions first

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -16,6 +16,7 @@ class DatabaseSeeder extends Seeder
     public function run(): void
     {
         $this->call([
+            SyriaRegionsSeeder::class,
             ComprehensiveSeeder::class,
         ]);
     }
