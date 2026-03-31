ba53e7f1671827fe6c4c03f28535c3c347d2339f
waad <waad87770@gmail.com>
2026-03-31 16:05:38 +0300

    Update Firebase config to almiftah-real-estate project

--- config/firebase.php
+++ config/firebase.php
@@ -1,8 +1,8 @@
-<?php
+﻿<?php
 
 return [
-    'project_id' => 'almoftahapp',
-    'client_email' => 'firebase-adminsdk-fbsvc@almoftahapp.iam.gserviceaccount.com',
+    'project_id' => 'almiftah-real-estate',
+    'client_email' => 'firebase-adminsdk-fbsvc@almiftah-real-estate.iam.gserviceaccount.com',
     'private_key' => env('FIREBASE_PRIVATE_KEY', ''),
     'credentials_file' => base_path('firebase-credentials.json'),
 ];
